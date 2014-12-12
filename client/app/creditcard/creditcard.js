bac1889215254a6fa7477c9b6732e10e7f2bd2a9
Issac <issac@Issacs-MacBook-Air.local>
2014-12-11 21:36:37 -0800

    saves credit card data now

--- client/app/creditcard/creditcard.js
+++ client/app/creditcard/creditcard.js
@@ -10,6 +10,19 @@ angular.module('pledgr.creditcard', [])
     expmonth: "",
     expyear: ""
   };
+  $scope.getCards = function() {
+    $http.get('/card/get')
+    .success(function (res) {
+      for(var i = 0; i < res.data.length; i++) {
+          $scope.cards.push(res.data[i]);
+      } 
+    })
+    .error(function(error,status){
+      console.log(error,status);
+    });
+  };
+  $scope.getCards();
+
   $scope.addCard = function() {
      var $form = $('#payment-form');
      Stripe.card.createToken($form, 
@@ -22,12 +35,19 @@ angular.module('pledgr.creditcard', [])
             $http({
               url: '/card/add',
               method: "POST",
-              data: {user: 'jsakdfj@hackreactor.com', stripeToken: token},
+              data: {
+                user: 'jsakdfj@hackreactor.com', 
+                stripeToken: token,
+                endingDigits: $scope.formData.number.slice(-4),
+                exp: ($scope.formData.expmonth 
+                    + '/' + $scope.formData.expyear)
+              },
               headers: {'Content-Type': 'application/json'}
             })
             .then(function (res) {
+                  console.log(res.data);
                 if(res.data === "SUCCESS") {
-                  $scope.cards.push({number: 'Card ending in ' + $scope.formData.number.slice(-4), exp: $scope.formData.expmonth 
+                  $scope.cards.push({endingDigits: 'Card ending in ' + $scope.formData.number.slice(-4), exp: $scope.formData.expmonth 
                     + '/' + $scope.formData.expyear});
                 }
             });
@@ -36,6 +56,24 @@ angular.module('pledgr.creditcard', [])
           }
         }
      ); 
+  };
+
+  $scope.deleteCard = function(card) {
+    $http({
+      url: '/card/delete',
+      method: "POST",
+      data: {user: 'jsakdfj@hackreactor.com', endingDigits: card.endingDigits},
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(function (res) {
+      if(res.data === "SUCCESS") {
+        for(var i = 0; i < $scope.cards.length; i++) {
+          if($scope.cards[i].number === card.number) {
+            $scope.cards.splice(i, 1);
+          }
+        }
+      }
+    });      
   }
 
 });
